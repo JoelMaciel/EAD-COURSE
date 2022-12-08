0a51a6ba0e7da5c929b778db4eb5925ed2c60774
Joel Maciel <soldado25461@gmail.com>
2022-12-08 20:17:31 -0300

    feat: Creating crud for the lesson entity with validations

--- src/main/java/com/ead/course/services/LessonService.java
+++ src/main/java/com/ead/course/services/LessonService.java
@@ -1,4 +1,18 @@
 package com.ead.course.services;
 
+import com.ead.course.models.LessonModel;
+import com.ead.course.models.ModuleModel;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 public interface LessonService {
+    LessonModel save(LessonModel lesson);
+
+    Optional<LessonModel> findLessonIntoModule(UUID moduleId, UUID lessonId);
+
+    void delete(LessonModel lessonModel);
+
+    List<LessonModel> findAllModule(UUID moduleId);
 }
