ccd8ba69b18ac3f340b30c7c03f111bf34f1e4be
Joel <joel.viana@audora.com.br>
2023-11-15 15:20:25 -0300

    fix: change port container 3307

--- src/test/java/com/ead/course/containers/DatabaseContainerConfiguration.java
+++ src/test/java/com/ead/course/containers/DatabaseContainerConfiguration.java
@@ -11,11 +11,16 @@ public class DatabaseContainerConfiguration {
     public static MySQLContainer<?> mysqlContainer = new MySQLContainer<>("mysql:8.0")
             .withDatabaseName("ead_course_two_test")
             .withUsername("root")
-            .withPassword("root");
+            .withPassword("root")
+            .withExposedPorts(3307);
 
     @DynamicPropertySource
     public static void overrideProps(DynamicPropertyRegistry registry) {
-        registry.add("spring.datasource.url", mysqlContainer::getJdbcUrl);
+        String jdbcUrl = String.format("jdbc:mysql://%s:%d/ead_course_two_test",
+                mysqlContainer.getHost(),
+                mysqlContainer.getMappedPort(3307));
+
+        registry.add("spring.datasource.url", () -> jdbcUrl);
         registry.add("spring.datasource.username", mysqlContainer::getUsername);
         registry.add("spring.datasource.password", mysqlContainer::getPassword);
     }
